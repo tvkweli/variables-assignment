1e49472ce18b9342a8561d9cc719f4013b74ff1c
Talib Visram <talibvisram@Talibs-MacBook-Pro.local>
2018-03-19 12:02:47 -0400

    variables

---
+++ main.js
@@ -0,0 +1,10 @@
+var grant2018 = 1000, //$1000 all family members get in 2018
+	var familyMembers = {
+		number: ["1", "2", "3", "4", "5"],
+		funds: [800, 950, 1000, 1100, 1200]
+	}; //funds for family members depending on size of family
+var NGOs = ["Help Unlimited", "New Start", "Better Tomorrow", "Home Safe", "New Home"] //names of NGOs
+var English {
+	yes = 1,
+		no = 2
+}; //does the head of household speak English?
